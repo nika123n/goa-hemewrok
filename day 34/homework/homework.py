63a4c9ed6dfb1ce68feb9ee178a017c3d56a974c
nika123n <nikoloznikoloz234@gmail.com>
2024-06-06 18:47:32 +0400

    sixth commit

---
+++ day 34/homework/homework.py
@@ -0,0 +1,8 @@
+# 2) შექმნათ სია სადაც ჩაამატებთ თქვენი ყველა ჯგუფელის სახელს და გვარს, შემდეგ კი დაბეჭდავთ შუა 10 მოსწავლეს
+
+members =["andria qorchilava", "andria alavidze", "sandro chapidze", "giviko yvavrishvili", "mate kotorashvili", "gega diasamnidze", "luka natroshvili", "anastasia xvedelidze", "giorgi shavadze",
+"gabriel rviniashvili", "toma omanashvili", "luka chaxvashvili", "erekle yaragozishvili", "levan samsonidze", "andria mekaluashvili", "luxumi zurabiani", "tazo kldiashvili", "mate tolordava",
+ "davit cixelashvili", "sava lomsadze", "saba nakashidze", "nika kuwashvili", "mishiko berelidze", "vika gvilia", "mariam zardiashvili"]
+
+
+print(members[6:16])
